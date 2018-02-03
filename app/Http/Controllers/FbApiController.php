b81379285638ca78c2df962d6075c8001e808990
Triet Le <funnyguy@gmail.com>
2018-02-03 16:25:13 +0700

    init

---
+++ app/Http/Controllers/FbApiController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class FbApiController extends Controller {
+
+    public function __construct()
+    {
+
+    }
+
+    public function test() {
+
+    }
+}
